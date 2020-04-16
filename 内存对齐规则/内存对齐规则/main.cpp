51f4f0cea37f87c587f077ea53c241c02dee5e8e
GeniusPanYiHao <1542029272@qq.com>
2020-04-16 21:06:12 +0800

    结构体大小计算

---
+++ 内存对齐规则/内存对齐规则/main.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+//8
+struct A
+{
+public:
+	int a;
+	char b;
+	short c;
+};
+//12
+struct B
+{
+public:
+	char b;
+	int a;
+	short c;
+};
+//8
+struct C
+{
+public:
+	short c;
+	char b;
+	int a;
+};
+//24
+struct D
+{
+public:
+	short c;
+	char b;
+	double d;
+	int a;
+};
+int main()
+{
+	cout << sizeof(A) << endl;
+	cout << sizeof(B)<< endl;
+	cout << sizeof(C) << endl;
+	cout << sizeof(D) << endl;
+}
