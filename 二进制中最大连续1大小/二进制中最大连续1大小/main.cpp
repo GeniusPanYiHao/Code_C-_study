21a6f591d646a495a30795e0e1090920132192fc
GeniusPanYiHao <1542029272@qq.com>
2020-06-14 18:57:48 +0800

    题

---
+++ 二进制中最大连续1大小/二进制中最大连续1大小/main.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int count = 0;
+	int sum = 0;
+	while (n)
+	{
+		if (n & 1)
+		{
+			count++;
+			if (count > sum)
+			{
+				sum = count;
+			}
+
+		}
+		else
+			count = 0;
+		n >>= 1;
+	}
+	cout << sum << endl;
+}
