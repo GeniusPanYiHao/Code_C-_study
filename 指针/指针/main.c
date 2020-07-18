8916c84aa061a8213222b01caf86317842b1c5df
GeniusPanYiHao <1542029272@qq.com>
2020-07-18 18:59:56 +0800

    指针

--- 指针/指针/main.c
+++ 指针/指针/main.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 int main()
 {
-	int n = 10;
+	/*int n = 10;
 	char* pc = (char*)&n;
 	int* pi = &n;
 
@@ -10,6 +10,11 @@ int main()
 	printf("%p\n", pc);
 	printf("%p\n", pc+1);
 	printf("%p\n", pi);
-	printf("%p\n", pi+1);
-
+	printf("%p\n", pi+1);*/
+	int arr[5] = { 0, 1, 2, 3, 4 };
+	int *p = arr;//:指针存放数组的首元素地址。p+i变为数组下标为i
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d\n",*(p + i));
+	}
 }
