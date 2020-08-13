479b15d6fe72b0931895d76cdf45e53da586bae4
GeniusPanYiHao <1542029272@qq.com>
2020-08-13 13:44:54 +0800

    判断当前机器大小端

---
+++ 检测大小端/检测大小端/main.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 1;
+	char b = (char)a;
+	if (b == 1)
+		cout << "Ð¡¶Ë" << endl;
+	else
+		cout << "´ó¶Ë" << endl;
+}
