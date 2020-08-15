fdaeaaae65c95d309c70d5388fd974f785b7b9fa
GeniusPanYiHao <1542029272@qq.com>
2020-08-15 22:21:12 +0800

    指针

--- 指针/指针/main.c
+++ 指针/指针/main.c
@@ -1,7 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+struct Test
+{
+	int num;
+	char pName;
+	short sData;
+	char cha[2];
+	short s[4];
+}*p;
 int main()
 {
+	printf("%p\n", p);
+	printf("%p\n", p + 0x1);
+	printf("%p\n", (unsigned long)p + 0x1);
+	printf("%p\n", (unsigned int*)p + 0x1);
 	/*int n = 10;
 	char* pc = (char*)&n;
 	int* pi = &n;
@@ -18,7 +30,7 @@ int main()
 	//	printf("%d\n",*(p + i));
 	//}
 
-	int a[5] = { 1, 2, 3, 4, 5 };
+	/*int a[5] = { 1, 2, 3, 4, 5 };
 	int *ptr = (int*)(&a + 1);
-	printf("%d%d", *(a + 1), *(ptr - 1));
+	printf("%d%d", *(a+1), *(ptr - 1));*/
 }
